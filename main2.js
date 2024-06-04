8ba34fd0caf941bc278891d6ba14d61dbde387f1
Daniil Patckin <126461411+patckin-d@users.noreply.github.com>
2024-06-05 00:56:29 +0400

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,25 @@
+let text="Hello, world";
+console.log(text);
+
+let numb = 12;
+console.log(numb);
+
+let _true = true;
+console.log(_true);
+
+let underf = undefined;
+console.log(underf);
+
+let _null = null;
+console.log(_null);
+
+let mass = [underf,_null, numb, "hohoho"];
+console.log(mass);
+
+let objectt = {
+    color:"green",
+    name: "Antonio"
+}
+console.log(objectt)
+
+
